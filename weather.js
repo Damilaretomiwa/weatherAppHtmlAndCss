17d07a665d1a20da99e397b13b0510f02e5d37f7
whitewizardd <o.samuel@native.semicolon.africa>
2023-08-27 18:30:41 +0100

    weather update

--- weather.js
+++ weather.js
@@ -1,9 +1,22 @@
-
-
-
-
 let form = document.getElementById("use_press")
 let weather_forecast = new XMLHttpRequest();
+let key_I = "1dfb6d7b802060dd7cbf86392bbebb2d";
+let wea;
+let defaultDisplay = () => {
+    weather_forecast.open("GET", `https://api.openweathermap.org/data/2.5/weather?q=${"ikoyi"}&appid=${key_I}`)
+    weather_forecast.send()
+    weather_forecast.onload = () => {
+        if (weather_forecast.status === 200) {
+            wea = JSON.parse(weather_forecast.response)
+            document.getElementById("temperature").innerText = "temperature: " + wea.main.temp
+            document.getElementById("description").innerText = "weather description: " + wea.weather[0].description
+            document.getElementById("country name").innerText = "country: " + wea.sys.country
+        } else {
+            document.getElementById("country name").innerText = "Bad request gotten";
+        }
+    }
+}
+defaultDisplay()
 let userCityChoice = () => {
     let userCity = document.getElementById("city")
     return userCity.value
@@ -14,17 +27,12 @@ let getIt= ( city) => {
     weather_forecast.send()
     weather_forecast.onload = () => {
         if (weather_forecast.status === 200) {
-            console.log(weather_forecast.response)
-            let wea = JSON.parse(weather_forecast.response)
+            wea = JSON.parse(weather_forecast.response)
             document.getElementById("temperature").innerText = "temperature: "+wea.main.temp
             document.getElementById("description").innerText = "weather description: "+wea.weather[0].description
             document.getElementById("country name").innerText = "country: "+wea.sys.country
-            // console.log("Temperature is", wea.main.temp)
-            // console.log("Maximum Temp is", wea.main.temp_max)
-            // console.log("Minimum Temp is", wea.main.temp_min)
-            // console.log("weather description", wea.weather[0].description)
         } else {
-            console.log("Bad request gotten");
+            document.getElementById("country name").innerText = "Bad request gotten";
         }
     }
 }
