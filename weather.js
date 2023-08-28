867ae0e15811a1f079575012e360c4f77d707c38
whitewizardd <o.samuel@native.semicolon.africa>
2023-08-28 06:03:58 +0100

    weather update

--- weather.js
+++ weather.js
@@ -3,12 +3,15 @@ let weather_forecast = new XMLHttpRequest();
 let key_I = "1dfb6d7b802060dd7cbf86392bbebb2d";
 let wea;
 let defaultDisplay = () => {
-    weather_forecast.open("GET", `https://api.openweathermap.org/data/2.5/weather?q=${"ikoyi"}&appid=${key_I}`)
+    let date = new Date();
+    document.getElementById("date_time").innerText = date
+    console.log("Date is", date)
+    weather_forecast.open("GET", `https://api.openweathermap.org/data/2.5/weather?q=${"wuse"}&appid=${key_I}`)
     weather_forecast.send()
     weather_forecast.onload = () => {
         if (weather_forecast.status === 200) {
             wea = JSON.parse(weather_forecast.response)
-            document.getElementById("temperature").innerText = "temperature: " + wea.main.temp
+            document.getElementById("temperature").innerText = "temperature: " + wea.main.temp+"F"
             document.getElementById("description").innerText = "weather description: " + wea.weather[0].description
             document.getElementById("country name").innerText = "country: " + wea.sys.country
         } else {
@@ -28,10 +31,13 @@ let getIt= ( city) => {
     weather_forecast.onload = () => {
         if (weather_forecast.status === 200) {
             wea = JSON.parse(weather_forecast.response)
-            document.getElementById("temperature").innerText = "temperature: "+wea.main.temp
+            document.getElementById("temperature").innerText = "temperature: "+wea.main.temp+"F"
             document.getElementById("description").innerText = "weather description: "+wea.weather[0].description
             document.getElementById("country name").innerText = "country: "+wea.sys.country
+            console.log(wea.dt)
         } else {
+            document.getElementById("temperature").innerText = "Bad request gotten"
+            document.getElementById("description").innerText = "Bad request gotten"
             document.getElementById("country name").innerText = "Bad request gotten";
         }
     }
